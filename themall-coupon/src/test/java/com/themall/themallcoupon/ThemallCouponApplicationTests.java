31798d972fd9c70c3779950f76b595071f9294ee
一个小小卒 <3162628030@qq.com>
2021-05-22 20:47:21 +0800

    初始架构

---
+++ themall-coupon/src/test/java/com/themall/themallcoupon/ThemallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themall.themallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ themall-order/src/test/java/com/themall/themallorder/ThemallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themall.themallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
