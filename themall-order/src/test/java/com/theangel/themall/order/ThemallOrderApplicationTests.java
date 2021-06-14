5a674f6e074f045fe701ed82e280256f5b0bf619
theangel <3162628030@qq.com>
2021-06-14 21:30:11 +0800

    将所有微服务注册到腾讯云nacos1.4.1上

--- themall-order/src/test/java/com/theangel/themall/order/ThemallOrderApplicationTests.java
+++ themall-order/src/test/java/com/theangel/themall/order/ThemallOrderApplicationTests.java
@@ -1,13 +1,11 @@
 package com.theangel.themall.order;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ThemallOrderApplicationTests {
-
-    @Test
+    
     void contextLoads() {
     }
-
+    
 }
--- themall-product/src/test/java/com/theangel/themall/themallproduct/ThemallProductApplicationTests.java
+++ themall-product/src/test/java/com/theangel/themall/themallproduct/ThemallProductApplicationTests.java
@@ -1,19 +1,19 @@
 package com.theangel.themall.themallproduct;
 
 import com.theangel.themall.product.service.BrandService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ThemallProductApplicationTests {
-
+    
     @Autowired
     private BrandService brandService;
-
+    
     @Test
     void contextLoads() {
         System.out.println(brandService.list());
     }
-
+    
 }
