b312b75fac8b03b9060289da0f11b842d0f1d88b
poo0054 <poo0054@outlook.com>
2023-11-20 11:45:57 +0800

    fix(vue): 前端项目优化

--- renren-fast-vue/config/index.js
+++ renren-fast-vue/config/index.js
@@ -14,7 +14,7 @@ module.exports = {
     // 代理列表, 是否开启代理通过[./dev.env.js]配置
     proxyTable: devEnv.OPEN_PROXY === false ? {} : {
       '/proxyApi': {
-        target: 'http://themall-gatway:8080/renren-fast/',
+        target: 'http://localhost:8085/renren-fast/',
         changeOrigin: true,
         pathRewrite: {
           '^/proxyApi': '/'
