21e5d33af87c71f77896a1589c8e53dc9589034a
poo0054 <poo0054@outlook.com>
2023-11-22 09:08:49 +0800

    fix(gateway): 首页地址

--- themall-search/src/main/java/com/theangel/themall/search/config/ElasticSearchConfig.java
+++ themall-search/src/main/java/com/theangel/themall/search/config/ElasticSearchConfig.java
@@ -1,8 +1,14 @@
 package com.theangel.themall.search.config;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +27,12 @@ public class ElasticSearchConfig {
     @Value("${spring.elasticsearch.cluster.nodes}")
     private String[] urisCloud;
 
+    @Value("${spring.elasticsearch.cluster.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.cluster.password}")
+    private String password;
+
     static {
         RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
     /*    builder.addHeader("Authorization", "Bearer " + TOKEN);
@@ -34,7 +46,18 @@ public class ElasticSearchConfig {
     public RestHighLevelClient esClient() {
         // 创建多个HttpHost
         HttpHost[] httpHosts = Arrays.stream(urisCloud).map(HttpHost::create).toArray(HttpHost[]::new);
-        return new RestHighLevelClient(RestClient.builder(httpHosts));
+        RestClientBuilder builder = RestClient.builder(httpHosts);
+        if (ObjectUtils.allNotNull(username, password)) {
+            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));  //es账号密码（默认用户名为elastic）
+            builder.setHttpClientConfigCallback(httpClientBuilder -> {
+                httpClientBuilder.disableAuthCaching();
+                return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+            });
+        }
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(builder);
+        return restHighLevelClient;
     }
 
 }
