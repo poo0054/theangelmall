37ab88337bb4d8786c3b3c1dea083348e868f690
poo0054 <poo0054@outlook.com>
2023-12-14 17:11:49 +0800

    fix(renrenfast): 删除无用redis

--- renren-fast/src/main/java/io/renren/modules/sys/service/impl/SysConfigServiceImpl.java
+++ renren-fast/src/main/java/io/renren/modules/sys/service/impl/SysConfigServiceImpl.java
@@ -11,16 +11,12 @@ package io.renren.modules.sys.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.google.gson.Gson;
-import com.themall.model.exception.RRException;
 import io.renren.modules.sys.dao.SysConfigDao;
 import io.renren.modules.sys.entity.SysConfigEntity;
-import io.renren.modules.sys.redis.SysConfigRedis;
 import io.renren.modules.sys.service.SysConfigService;
 import io.renren.utils.PageUtils;
 import io.renren.utils.Query;
 import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,12 +26,6 @@ import java.util.Map;
 @Service
 public class SysConfigServiceImpl extends ServiceImpl<SysConfigDao, SysConfigEntity> implements SysConfigService {
 
-    private SysConfigRedis sysConfigRedis;
-
-    @Autowired
-    public void setSysConfigRedis(SysConfigRedis sysConfigRedis) {
-        this.sysConfigRedis = sysConfigRedis;
-    }
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -54,21 +44,18 @@ public class SysConfigServiceImpl extends ServiceImpl<SysConfigDao, SysConfigEnt
     @Override
     public void saveConfig(SysConfigEntity config) {
         this.save(config);
-        sysConfigRedis.saveOrUpdate(config);
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void update(SysConfigEntity config) {
         this.updateById(config);
-        sysConfigRedis.saveOrUpdate(config);
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void updateValueByKey(String key, String value) {
         baseMapper.updateValueByKey(key, value);
-        sysConfigRedis.delete(key);
     }
 
     @Override
@@ -76,34 +63,9 @@ public class SysConfigServiceImpl extends ServiceImpl<SysConfigDao, SysConfigEnt
     public void deleteBatch(Long[] ids) {
         for (Long id : ids) {
             SysConfigEntity config = this.getById(id);
-            sysConfigRedis.delete(config.getParamKey());
         }
 
         this.removeByIds(Arrays.asList(ids));
     }
 
-    @Override
-    public String getValue(String key) {
-        SysConfigEntity config = sysConfigRedis.get(key);
-        if (config == null) {
-            config = baseMapper.queryByKey(key);
-            sysConfigRedis.saveOrUpdate(config);
-        }
-
-        return config == null ? null : config.getParamValue();
-    }
-
-    @Override
-    public <T> T getConfigObject(String key, Class<T> clazz) {
-        String value = getValue(key);
-        if (StringUtils.isNotBlank(value)) {
-            return new Gson().fromJson(value, clazz);
-        }
-
-        try {
-            return clazz.newInstance();
-        } catch (Exception e) {
-            throw new RRException("获取参数失败");
-        }
-    }
 }
