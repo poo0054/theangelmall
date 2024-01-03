c84c06f1b42236c01c5d7c3784e3d1fa9742c1e4
poo0054 <poo0054@outlook.com>
2024-01-03 18:39:56 +0800

    fix: 添加@Mapper

--- themall-admin/src/main/java/com/themall/admin/dao/SysDeptDao.java
+++ themall-admin/src/main/java/com/themall/admin/dao/SysDeptDao.java
@@ -2,6 +2,7 @@ package com.themall.admin.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.themall.admin.pojo.entity.SysDept;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * (SysDept)表数据库访问层
@@ -9,6 +10,7 @@ import com.themall.admin.pojo.entity.SysDept;
  * @author makejava
  * @since 2024-01-03 17:54:38
  */
+@Mapper
 public interface SysDeptDao extends BaseMapper<SysDept> {
 
 }
