8d76f7eb0687fa10bc1567e3ecf723d6db06d2c4
poo0054 <poo0054@outlook.com>
2024-01-03 15:58:08 +0800

    fix: 删除renrenfast无用代码

---
+++ themall-admin/src/main/java/com/themall/admin/AdminApplication.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ * <p>
+ * https://www.renren.io
+ * <p>
+ * 版权所有，侵权必究！
+ */
+
+package com.themall.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author poo0054
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
